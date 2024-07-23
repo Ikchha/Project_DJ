24fc138f2bf3298a8dc30af0420b3d34a6a3913c
Ikchha <102365567+Ikchha@users.noreply.github.com>
2024-07-23 18:01:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 song1 = "";
 song2 = "";
-
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-
 scoreLeftWrist = 0;
-
-status = "";
+scoreRightWrist = 0;
+song1_status = "";
+song2_status = "";
 
 function preload() {
     song1 = loadSound("DieForYou.mp3");
     song2 = loadSound("yes_and.mp3");
 }
 function setup() {
-    canvas =  createCanvas(600, 500);
+    canvas =  createCanvas(400, 400);
     canvas = canvas.center();
 
-    video = creatCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
@@ -27,13 +26,44 @@ function setup() {
 function modelLoaded() {
     console.log("Model is initialized!")
 }
+function draw() {
+    image(video, 0, 0, 600, 500);
+    song1_status = song1.isPlaying();
+    fill("#cc1b14");
+    stroke("#faf3f2");
+    
+    if(scoreLeftWrist>0.2) {
+        circle(leftWristX, leftWristY, 20);
+        song2.stop();
+
+        if(song1_status==false){
+            song1.play();
+            document.getElementById("song").innerHTML = " Playing - Die For You ";
+
+        }
+    }
+
+    if(scoreRightWrist>0.2){
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if(song2_status==false){
+            song2.play();
+            document.getElementById("song").innerHTML = " Playing - Yes, and? "
+        }
+
+    }
+}
 function gotPoses(results) {
     if(results.length>0) {
-        scoreLeftWrist = results[0].pose.keypoints[9].score;
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score Left Wrist = " + scoreLeftWrist + "Score Right Wrist = " + scoreRightWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
@@ -42,21 +72,3 @@ function gotPoses(results) {
 
 }
 
-function draw() {
-    image(video, 0, 0, 600, 500);
-    status = song1.play();
-    fill("#cc1b14");
-    stroke("#faf3f2");
-    
-    if(scoreLeftWrist>0.2){
-        circle(leftWristX, leftWristY, 20);
-        song2.stop();
-        if(status != song1.isPlaying()){
-            song1.play();
-            document.getElementById("heading1").innerHTML = song1;
-
-        }
-
-
-    }
-}
